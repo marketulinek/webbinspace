d8d96340f50500b61d941f4e088fc77369e01e08
Marketa <habranova.marketa@gmail.com>
2022-08-20 13:57:41 +0200

    Added simple welcoming page. I used custom project-level approach for template structure.

--- webbinspace/settings.py
+++ webbinspace/settings.py
@@ -45,7 +45,8 @@ ROOT_URLCONF = 'webbinspace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
