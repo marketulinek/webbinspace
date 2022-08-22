8ec08a26c6584fb8c1cfc40ec239d9a6a8fba4ee
Marketa <habranova.marketa@gmail.com>
2022-08-22 21:55:44 +0200

    Adjusted model class diagram, created models and registered them in admin, made migration

--- docs/models.md
+++ docs/models.md
@@ -4,13 +4,17 @@
 ```mermaid
 classDiagram
     class Report{
-      package_number: int
+      package_number: str
       date_code: int
       cycle: int
       created_at: DateTime
+      __str__()
+      get_absolute_url()
     }
     class Category{
       name: str
+      __str__()
+      get_absolute_url()
     }
     class Visit{
       visit_id: str
@@ -19,8 +23,11 @@ classDiagram
       scheduled_start_time: DateTime
       duration: DateTime
       science_instrument_and_mode: str
+      instrument: tuple
+      mode: str
       target_name: str
       keywords: str
+      __str__()
     }
     Report "1" *-- "*" Visit
     Category "0..1" o-- "*" Visit
--- webb/models.py
+++ webb/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Report(models.Model):
+    package_number = models.CharField(max_length=10)
+    date_code = models.SmallIntegerField()
+    cycle = models.SmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%i_report_%i' % (self.package_number, self.date_code)
+
+    def get_absolute_url(self):
+        return reverse('report_detail', args=[self.date_code])
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category_detail', args=[self.name])
+
+class Visit(models.Model):
+    INSTRUMENT_CHOICES = (
+        ('1', 'NIRCam'),
+        ('2', 'NIRSpec'),
+        ('3', 'MIRI'),
+        ('4', 'NIRISS')
+    )
+
+    report = models.ForeignKey(
+        Report,
+        on_delete=models.PROTECT,
+        related_name='visits'
+    )
+    visit_id = models.CharField(max_length=10, default='')
+    pcs_mode = models.CharField(max_length=10, default='')
+    visit_type = models.CharField(max_length=30, default='')
+    scheduled_start_time = models.DateTimeField(null=True)
+    duration = models.DateTimeField(null=True)
+    science_instrument_and_mode = models.CharField(max_length=50, default='')
+    instrument = models.CharField(
+        choices=INSTRUMENT_CHOICES,
+        max_length=1,
+        default=''
+    )
+    mode = models.CharField(max_length=50, default='')
+    target_name = models.CharField(max_length=30, default='')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='visits',
+        null=True
+    )
+    keywords = models.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return self.visit_id
