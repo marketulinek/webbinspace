2247fbbc64725fac8002a8cef2f92fc765a271dd
Marketa <habranova.marketa@gmail.com>
2022-10-18 20:42:09 +0200

    Added test for fetching the correct instrument type.

--- webb/tests.py
+++ webb/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .management.commands.report_parser import get_instrument_type
 
-# Create your tests here.
+
+class ReportParserTests(TestCase):
+
+    def test_get_instrument_type(self):
+
+        self.assertEqual(get_instrument_type('NIRCam Imaging'), '1')
+        self.assertEqual(get_instrument_type('NIRSpec Dark'), '2')
+        self.assertEqual(get_instrument_type('MIRI External Flat'), '3')
+        self.assertEqual(get_instrument_type('NIRISS Single-Object Slitless Spectroscopy'), '4')
+        self.assertEqual(get_instrument_type('WFSC NIRCam Fine Phasing'), '1')
+        self.assertEqual(get_instrument_type('Station Keeping'), None)
+        self.assertEqual(get_instrument_type('FGS Internal Flat'), None)
+        self.assertEqual(get_instrument_type(''), None)
