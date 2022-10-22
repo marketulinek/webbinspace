590522f1a9914c888df7bf7a8c7755e228349493
Marketa <habranova.marketa@gmail.com>
2022-10-22 16:48:28 +0200

    Switched order of targets in homepage view.

--- webb/views.py
+++ webb/views.py
@@ -7,16 +7,18 @@ from .utils import get_observing_progress
 def homepage(request):
 
     prev_current_target = Visit.objects.filter(scheduled_start_time__lte=timezone.now()).order_by('-scheduled_start_time')[:2]
+    prev_target = prev_current_target[1]
+    current_target = prev_current_target[0]
     next_target = Visit.objects.filter(scheduled_start_time__gte=timezone.now()).order_by('scheduled_start_time')[:1]
 
     current_target_progress = get_observing_progress(
-        prev_current_target[0].scheduled_start_time,
-        prev_current_target[0].duration
+        current_target.scheduled_start_time,
+        current_target.duration
     )
 
     context = {
-        'prev_target': prev_current_target[0],
-        'current_target': prev_current_target[1],
+        'prev_target': prev_target,
+        'current_target': current_target,
         'next_target': next_target,
         'current_target_progress': current_target_progress
     }
