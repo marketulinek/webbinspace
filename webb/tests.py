2b7ab2e6d42bdff1e4b00a0bac4b374e1074280f
Marketa <habranova.marketa@gmail.com>
2022-10-22 16:08:08 +0200

    Added test for formatting start time and duration and test for getting column lengths.

--- webb/tests.py
+++ webb/tests.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Report, Visit
-from .management.commands.report_parser import get_instrument_type,format_duration
+from .management.commands.report_parser import get_instrument_type,format_duration,format_start_time,get_column_lengths
+import datetime
 
 
 class WebbTests(TestCase):
 
     def setUp(self):
         r = Report.objects.create(package_number='2219105f02', date_code='20220710', cycle=1)
-        
+
         Visit.objects.create(report=r, visit_id='2739:4:1', scheduled_start_time='2022-07-14T19:00:00Z', duration=format_duration('00/00:12:06'), target_name='Neptune', keywords='Planet')
         Visit.objects.create(report=r, visit_id='1022:9:5', scheduled_start_time='2022-07-15T19:00:00Z', duration=format_duration('00/00:12:06'), target_name='Jupiter', keywords='Planet')
 
@@ -34,3 +35,23 @@ class ReportParserTests(TestCase):
         self.assertEqual(get_instrument_type('Station Keeping'), None)
         self.assertEqual(get_instrument_type('FGS Internal Flat'), None)
         self.assertEqual(get_instrument_type(''), None)
+
+    def test_format_start_time(self):
+
+        self.assertEqual(format_start_time('2022-07-10T13:23:07Z'), '2022-07-10T13:23:07Z')
+        self.assertEqual(format_start_time('2022-07-10'), None)
+        self.assertEqual(format_start_time('some date and time'), None)
+        self.assertEqual(format_start_time(''), None)
+
+    def test_format_duration(self):
+
+        self.assertEqual(format_duration('00/00:16:55'), datetime.timedelta(minutes=16, seconds=55))
+        self.assertEqual(format_duration('01/17:24:00'), datetime.timedelta(days=1, hours=17, minutes=24, seconds=0))
+        self.assertEqual(format_duration(''), None)
+
+    def test_get_column_lengths(self):
+
+        self.assertEqual(get_column_lengths('----------  -------  -----------'), [10, 7, 11])
+        self.assertEqual(get_column_lengths('-  --  ---  ----  -----'), [1, 2, 3, 4, 5])
+        self.assertEqual(get_column_lengths('------'), [6])
+        self.assertEqual(get_column_lengths(''), [0])
