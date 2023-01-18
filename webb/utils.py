af9f8c7f68294fc15c8a27727f26238b506c6587
MarketaH <habranova.marketa@gmail.com>
2023-01-18 22:09:07 +0100

    Added function for adjusting the given number by the chosen range. It is used for getting the observing progess.

--- webb/utils.py
+++ webb/utils.py
@@ -1,19 +1,23 @@
 import datetime as dt
 
 
+def adjust_number_by_range(number, lowest_number, highest_number):
+    if number > highest_number:
+        return highest_number
+    if number < lowest_number:
+        return lowest_number
+    return number
+
+
 def get_observing_progress(start_time, duration):
-    """Returns the integer representing the percentage of target's observing progress.
-    
+    """Returns the integer representing
+    the percentage of target's observing progress.
+
     Ensures that the returned value is between 0 and 100.
     """
     percentage = calculate_time_progress(start_time, duration)
+    return adjust_number_by_range(percentage, 0, 100)
 
-    if percentage > 100:
-        return 100
-    elif percentage < 0:
-        return 0
-    else:
-        return percentage
 
 def calculate_time_progress(start_time, duration):
     """Calculates the progress over a period of time.
